e72f6bbe7a9cb3cdf6b83b76380824f19838f2f0
Mahad Ahmed <mahmed3@cnm.edu>
2021-10-06 10:10:20 -0600

    Functions

--- functions-1/functions-1.js
+++ functions-1/functions-1.js
@@ -1,14 +1,20 @@
 // Function #1
 // Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
-
- function square(number) {
-     return number * number;
+//Let number1 =
+//let number2 =
+ function square(number1, number2) {
+     return number1 * number2;
  }
 
- console.log(square(number:2))
+ console.log(square(2, 3))
 
 
 
 // 	Function #2
 // Take two strings as arguments and return a value that has them concatenated.
 
+function concat(string1, string2){
+    return string1 + String2;
+}
+
+console.log(concat(dkfjdk, djkfjdkf))
