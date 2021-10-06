96318687bf6a0c4ef4fe08e1888c5dcfedb23ae7
Mahad Ahmed <mahmed3@cnm.edu>
2021-10-06 09:46:14 -0600

    Functions

---
+++ data-types/data-types.js
@@ -0,0 +1,20 @@
+const name = 'Fred' //string data type b/c of ' '
+const anotherName ="Jim" // "" also work for strings
+const yetAnotherName = `Mary and ${name}`
+
+
+
+const age = 18 //number data type
+
+const zipCode = '87109'
+
+console.log( 1 + zipCode) //prints values as are and puts them together
+
+const cars = ['ford', 'dodge', 'toyota'] //array values are [0, 1, 2]
+const empty = []
+console.log(cars[0])
+console.log(cars[2])
+
+
+const person = {name: 'Fred', age: 18}
+
---
+++ functions-1/functions-1.js
@@ -0,0 +1,14 @@
+// Function #1
+// Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
+
+ function square(number) {
+     return number * number;
+ }
+
+ console.log(square(number:2))
+
+
+
+// 	Function #2
+// Take two strings as arguments and return a value that has them concatenated.
+
